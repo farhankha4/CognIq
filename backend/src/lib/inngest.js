e98ef4d2343cd4837662bd78a51811a3b9ea447e
farhankha4 <farhankha2020@gmail.com>
2026-05-13 16:08:51 +0530

    fixed inngest code

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -5,12 +5,21 @@ import User from "../models/User.js";
 export const inngest = new Inngest({ id: "BrainBridge" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+
   async ({ event }) => {
     await connectDB();
 
-    const { id, email_addresses, first_name, last_name, image_url } = event.data;
+    const {
+      id,
+      email_addresses,
+      first_name,
+      last_name,
+      image_url,
+    } = event.data;
 
     const newUser = {
       clerkId: id,
@@ -20,21 +29,21 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
-
-    // todo: do sth
   }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user-from-db",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+
   async ({ event }) => {
     await connectDB();
 
     const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
 
-    // todo: do sth else
+    await User.deleteOne({ clerkId: id });
   }
 );
 
