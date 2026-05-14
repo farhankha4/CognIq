113a77310d3b90b6887d0e941ba2d0018ddabce1
farhankha4 <farhankha2020@gmail.com>
2026-05-14 12:52:44 +0530

    updated vercel.json , latest version of inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,14 +34,14 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
-  } catch (error) {
-    console.error("💥 Error starting the server", error);
-  }
-};
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+//     app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+//   } catch (error) {
+//     console.error("💥 Error starting the server", error);
+//   }
+// };
 
 await connectDB();
 
