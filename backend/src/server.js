61d19108f23efe25e45179fc21198e00cf3224b6
farhankha4 <farhankha2020@gmail.com>
2026-05-19 21:57:30 +0530

    server side auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import { serve } from "inngest/express";
 import { inngest ,functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -14,16 +16,16 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/chat", chatRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "this is the books endpoint" });
-});
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
